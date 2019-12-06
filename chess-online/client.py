63ebb2ec38b984f5f70fc94d45ec292249387e92
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2019-12-07 02:42:47 +0530

    .

--- chess-online/client.py
+++ chess-online/client.py
@@ -8,20 +8,6 @@ height = 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Client')
 
-clientNumber = 0
-
-
-def read_pos(str):
-    print('str', str)
-    str = str.split(",")
-    print('str', str)
-    print('int', int(str[0]), int(str[1]))
-    return int(str[0]), int(str[1])
-
-
-def make_pos(tup):
-    return str(tup[0]) + ',' + str(tup[1])
-
 
 def redrawWindow(win, Player, Player2):
     win.fill((255, 255, 255))
@@ -34,20 +20,12 @@ def redrawWindow(win, Player, Player2):
 def main():
     run = True
     n = Network()
-    startpos = read_pos(n.getPos())
-    p = Player(startpos[0], startpos[1], 100, 100, (0, 255, 0))
-    p2 = Player(0, 0, 100, 100, (255, 255, 0))
-
+    p = n.getP()
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
-        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
-        p2.x = p2Pos[0]
-        p2.y = p2Pos[1]
-
-        p2.update()
-
+        p2 = n.send(p)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
--- chess-online/server.py
+++ chess-online/server.py
@@ -1,9 +1,11 @@
 import socket
 from _thread import *
 import sys
+from player import Player
+import pickle
 
 # private ip of server machine and port to run on network
-server = "192.168.1.2"
+server = "192.168.1.6"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,38 +19,30 @@ s.listen(2)
 print('Waiting for connection , server started')
 
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-
-def make_pos(tup):
-    return str(tup[0]) + ',' + str(tup[1])
-
-
-pos = [(0, 0), (100, 100)]
+players = [Player(0, 0, 50, 50, (255, 0, 0)),
+           Player(100, 100, 50, 50, (0, 0, 255))]
 
 
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 elif player == 0:
-                    reply = pos[1]
+                    reply = players[1]
                 print("Recieved: ", data)
                 print("Sending: ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
 
         except:
             break
