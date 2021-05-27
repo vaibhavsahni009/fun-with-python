3a0e0af53df04ac2197d3b6be51adece8974c113
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2021-05-27 22:57:56 +0530

    just a square

---
+++ turtle/challenge.py
@@ -0,0 +1,22 @@
+#for revision
+
+from turtle import Turtle,Screen
+
+
+
+
+t=Turtle()
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+
+
+
+screen=Screen()
+screen.exitonclick()
