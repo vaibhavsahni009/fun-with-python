a886ee198e29549f4980a10a899b9b1243b7ed26
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2021-05-30 21:13:41 +0530

    challenge

--- turtle/challenge.py
+++ turtle/challenge.py
@@ -1,18 +1,17 @@
 #for revision
 
 from turtle import Turtle,Screen
-
-
+import random 
 
 
 t=Turtle()
-
-for _ in range(50):
-    t.forward(10)
-    t.pu()
-    t.forward(10)
-    t.pd()
-
+t.width(15)
+for i in range(1,8):
+    
+    t.pencolor(random.choice(['green','blue','red','purple','teal','black']))
+    for j in range(i+2):
+        t.forward(100)
+        t.rt(360/(i+2))
 
 
 screen=Screen()
